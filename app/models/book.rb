b7ee789ac216129a0584a4927f1645ca6be5c115
dalach <wikadalach@gmail.com>
2014-06-04 23:50:15 +0200

    validate book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
   mount_uploader :cover_photo, CoverPhotoUploader
   has_and_belongs_to_many :bookshelves
+  validates :title, :author, presence: true
+  validates_format_of :cover_photo, with: %r{\.gif|jpg|png}i, :message => "Upload a cover photo jpg/jpeg/png"
 end
