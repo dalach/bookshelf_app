cf9f56d543f825bd2f6a53fe11b2432a6a1457ac
dalach <wikadalach@gmail.com>
2014-06-10 13:50:54 +0200

    test Book model

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should be valid" do
+    assert books(:one).valid?
+  end
+
+  test "shoud have a title" do
+    books(:one).title = nil
+    assert_not books(:one).valid?
+  end
+
+  test "should have an author" do
+    books(:one).author = nil
+    assert_not books(:one).valid?
+  end
+
+  test "should accept a book without cover_photo" do
+    books(:one).cover_photo = nil
+    assert books(:one).valid?
+  end
+
+  test "should not accept cover_photo other than .png/jpg/jpeg" do
+    books(:one).cover_photo = 'text.txt'
+    assert books(:one).valid?
+  end
 end
